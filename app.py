5bccc126a4e5d27d772cd8b6170038eb36952c69
Ramlavan Arumugasamyi <157941360+Ramlavn@users.noreply.github.com>
2024-05-23 00:00:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,7 @@ if selected_file != 'Choose a file':
 
     # Layout with two columns for selecting X-axis, Y-axis, and plot type
     col1, col2 = st.columns(2)
-
-    with col1:
+ with col1:
         # Dropdown for selecting X-axis
         x_axis = st.selectbox('Select the X-axis', options=["None"] + columns, key='x_axis')
         # Dropdown for selecting Y-axis
@@ -59,26 +58,44 @@ if selected_file != 'Choose a file':
     if st.button('Generate Plot'):
         # Function to generate the selected plot
         def generate_plot(df, x_axis, y_axis, plot_type):
+            fig, ax = plt.subplots(figsize=(6, 4))
+
             if plot_type == 'Line Plot':
-                st.line_chart(df[[x_axis, y_axis]])
+                sns.lineplot(x=df[x_axis], y=df[y_axis], ax=ax)
             elif plot_type == 'Bar Chart':
-                st.bar_chart(df[[x_axis, y_axis]])
+                sns.barplot(x=df[x_axis], y=df[y_axis], ax=ax)
             elif plot_type == 'Scatter Plot':
-                st.scatter_chart(df[[x_axis, y_axis]])
+                sns.scatterplot(x=df[x_axis], y=df[y_axis], ax=ax)
             elif plot_type == 'Distribution Plot':
                 if x_axis != "None":
-                    st.line_chart(df[x_axis].value_counts())
+                    sns.histplot(df[x_axis], kde=True, ax=ax)
                 else:
                     st.error("Please select a column for the X-axis.")
                     return
             elif plot_type == 'Count Plot':
                 if x_axis != "None":
-                    st.bar_chart(df[x_axis].value_counts())
+                    sns.countplot(x=df[x_axis], ax=ax)
+                    plt.xlabel(x_axis, fontsize=10)
                 elif y_axis != "None":
-                    st.bar_chart(df[y_axis].value_counts())
+                    sns.countplot(y=df[y_axis], ax=ax)
+                    plt.ylabel(y_axis, fontsize=10)
                 else:
                     st.error("Please select a column for either the X-axis or Y-axis.")
                     return
+                plt.ylabel('Count', fontsize=10)
+
+            # Adjust plot settings
+            ax.tick_params(axis='x', labelsize=10)  # Adjust x-axis label size
+            ax.tick_params(axis='y', labelsize=10)  # Adjust y-axis label size
+
+            # Set the title and axis labels
+            if plot_type != 'Count Plot':
+                plt.title(f'{plot_type} of {y_axis} vs {x_axis}', fontsize=12)
+                plt.xlabel(x_axis, fontsize=10)
+                plt.ylabel(y_axis, fontsize=10)
+
+            # Display the plot
+            st.pyplot(fig)
 
         # Generate the plot based on user selection
         generate_plot(df, x_axis, y_axis, plot_type)
