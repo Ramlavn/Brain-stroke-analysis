ce33f0f0d4755a9b2257afb2b77fb1f8f3c42a82
Ramlavan Arumugasamyi <157941360+Ramlavn@users.noreply.github.com>
2024-05-23 00:15:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title='Data Visualizer',
 # Author name
 author_name = "Ramlavan"
 # Current date
-current_date = datetime.today().strftime("%B %d, %Y")
+current_date = datetime.date.today().strftime("%B %d, %Y")
 
 # Author name and current date above the title
 st.write(f"Author: {author_name}")
